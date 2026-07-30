228ad594faf064ea14b642db6f8bc0453f59f422
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-07-30 23:29:13 +0530

    Writing sample mp3 chunks to a json file

---
+++ stt.py
@@ -0,0 +1,23 @@
+import whisper
+import json
+
+model = whisper.load_model("medium")
+
+
+result=model.transcribe(audio="audio/sample.mp3", language="en", task="transcribe" , word_timestamps=False)
+
+# print(result["segments"])
+chunks=[]
+
+for segment in result["segments"]:
+    chunk = {
+        "start": segment["start"],
+        "end": segment["end"],
+        "text": segment["text"]
+    }
+    chunks.append(chunk)
+
+print(chunks)
+
+with open("output.json", "w") as f:
+    json.dump(chunks, f)
