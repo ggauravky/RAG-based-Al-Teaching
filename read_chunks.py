81495e6349895b2111f065d2f94956a1aad79c35
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-07-31 17:53:46 +0530

    Bulk Embeddings & Chunk Loading with Pandas

--- read_chunks.py
+++ read_chunks.py
@@ -1,15 +1,36 @@
 import requests
+import os
+import json
+import numpy as np
+import pandas as pd
 
 
-def create_embedding(prompt: str):
-    r = requests.post(
-        "http://localhost:11434/api/embeddings",
-        json={"model": "bge-m3", "prompt": prompt},
-    )
+def create_embedding(text_list):
+    r = requests.post("http://localhost:11434/api/embed", json={
+        "model": "bge-m3",
+        "input": text_list
+    })
 
-    embedding = r.json()["embedding"]
-
+    embedding = r.json()["embeddings"] 
     return embedding
 
-a=create_embedding("Gaurav is a good boy")
-print(a)
+
+jsons = os.listdir("jsons")  
+my_dicts = []
+chunk_id = 0
+
+for json_file in jsons:
+    with open(f"jsons/{json_file}") as f:
+        content = json.load(f)
+    print(f"Creating Embeddings for {json_file}")
+    embeddings = create_embedding([c['text'] for c in content['chunks']])
+       
+    for i, chunk in enumerate(content['chunks']):
+        chunk['chunk_id'] = chunk_id
+        chunk['embedding'] = embeddings[i]
+        chunk_id += 1
+        my_dicts.append(chunk) 
+
+df = pd.DataFrame.from_records(my_dicts)
+print(df.head())
+
